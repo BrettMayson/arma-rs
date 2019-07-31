c15b0630a781e5ba122cbbf5ece61bc1f15c7826
Brett <brett@bmandesigns.com>
2019-07-31 15:41:45 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,128 @@
+# arma-rs
+
+The easiest way to make extensions for Arma 3.
+
+## Installation
+
+```toml
+[dependencies]
+arma-rs = "*"
+```
+
+## Usage
+
+### Hello World
+
+```rust
+use arma_rs::{rv, rv_handler};
+
+#[rv]
+fn hello() -> &'static str {
+    "Hello from Rust!"
+}
+
+#[rv_handler]
+fn init() {
+    println!("Ready to go!");
+}
+
+```
+
+Functions can easily be created by using the `rv` attribute. Every extension needs to have a `init()` function with the `rv_handler` attribute. `init()` is called when Arma 3 checks the version number of your extension.  
+If you do not require an `init()` just use an empty function.
+
+### Arguments
+
+```rust
+#[rv]
+fn say_hello(name: String) -> String {
+    format!("Hello {}", name)
+}
+```
+
+`"myExtension" callExtension ["say_hello", ["Rust"]]` => `Hello Rust`
+
+Any type that implements the trait `std::str::FromStr` can be used as an argument.  
+Any type that implements the trait `std::str::ToStr` can be used as the return type.
+
+```rust
+#[rv]
+fn is_arma3(version: u8) -> bool {
+    version == 3
+}
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Example
+
+### With arma-rs
+```rust 
+use arma_rs::{rv, rv_handler};
+
+#[rv]
+fn hello() -> &'static str {
+    "Hello from Rust!"
+}
+
+#[rv]
+fn is_arma3(version: u8) -> bool {
+    version == 3
+}
+
+#[rv_handler]
+fn init() {
+    println!("Ready to go!");
+}
+```
+
+### Without arma-rs
+```rust
+extern crate libc;
+use libc::c_char;
+use libc::strncpy;
+
+use std::ffi::CStr;
+use std::ffi::CString;
+
+#[no_mangle]
+pub unsafe extern "stdcall" fn RvExtensionVersion(output: *mut c_char, output_size: usize) {
+  let v = get_c(env!("CARGO_PKG_VERSION"));
+  strncpy(output, v, output_size);
+}
+
+#[no_mangle]
+pub unsafe extern "stdcall" fn RVExtension(output: *mut c_char, output_size: usize, function: *mut c_char ) {
+  let size = output_size - 1;
+  let r_function = CStr::from_ptr(function).to_str().unwrap();
+  match r_function {
+    "hello" => {
+      strncpy(output, CString::new("Hello from Rust!").unwrap().into_raw(), size);
+    },
+    _ => {
+      strncpy(output, CString::new("unknown").unwrap().into_raw(), size);
+    }
+  }
+}
+
+#[no_mangle]
+pub unsafe extern "stdcall" fn RVExtensionArgs(output: *mut c_char, output_size: usize, function: *mut c_char, args: *mut *mut c_char, arg_count: usize) {
+  let size = output_size - 1;
+  let r_function = CStr::from_ptr(function).to_str().unwrap();
+  match r_function {
+    "is_arma3" => {
+      if arg_count != 1 {
+        strncpy(output, CString::new(format!("Unexpected arg count: {}", arg_count)).unwrap().into_raw(), size);
+      } else {
+        let argv: &[*mut c_char; 1] = std::mem::transmute(args);
+        let version = u8::from_str(CStr::from_ptr(argv[0]).to_str().unwrap().replace("\"","")).unwrap();
+        strncpy(output, CString::new((version == 3).to_string()).unwrap().into_raw(), size);
+      }
+    },
+    _ => {
+      strncpy(output, CString::new("unknown").unwrap().into_raw(), size);
+    }
+  }
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,171 @@
+#![recursion_limit="256"]
+
+use std::sync::Mutex;
+use std::collections::HashMap;
+
+extern crate proc_macro;
+#[macro_use]
+extern crate lazy_static;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::ItemFn;
+
+lazy_static! {
+    static ref PROXIES:     Mutex<Vec<String>> = Mutex::new(Vec::new());
+    static ref PROXIES_ARG: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
+
+#[proc_macro_attribute]
+pub fn rv(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input!(item as ItemFn);
+
+    let name = &ast.ident;
+    let sname = ast.ident.to_string();
+    let handler = syn::Ident::new(&format!("{}_handler", name), name.span());
+    let info = syn::Ident::new(&format!("{}_info", name), name.span());
+
+    let mut args: HashMap<syn::Ident, syn::Type> = HashMap::new();
+    let mut argtypes: Vec<syn::Type> = Vec::new();
+
+    let astargs = &ast.decl.inputs;
+    astargs.pairs().for_each(|p| {
+        let v = p.value();
+        match v {
+            syn::FnArg::Captured(cap) => {
+                match &cap.pat {
+                    syn::Pat::Ident(ident) => {
+                        args.insert(ident.ident.clone(), cap.ty.clone());
+                        argtypes.push(cap.ty.clone());
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
+    });
+
+    let argcount = args.len();
+
+    let handlerfn = if args.is_empty() {
+        match ast.decl.output {
+            syn::ReturnType::Default => quote! { 
+                unsafe fn #handler(output: *mut libc::c_char, size: usize, _: Option<*mut *mut i8>, _: Option<usize>) {
+                    #name();
+                }
+            },
+            _ => quote! { 
+                unsafe fn #handler(output: *mut libc::c_char, size: usize, _: Option<*mut *mut i8>, _: Option<usize>) {
+                    libc::strncpy(output, std::ffi::CString::new(#name().to_string()).unwrap().into_raw(), size);
+                }
+            }
+        }
+    } else {
+        match ast.decl.output {
+            syn::ReturnType::Default => quote! { 
+                unsafe fn #handler(output: *mut libc::c_char, size: usize, args: Option<*mut *mut i8>, count: Option<usize>) {
+                    let argv: &[*mut libc::c_char; #argcount] = std::mem::transmute(args.unwrap());
+                    let mut argv: Vec<String> = argv.to_vec().into_iter().map(|s| std::ffi::CStr::from_ptr(s).to_str().unwrap().replace("\"", "")).collect();
+                    argv.reverse();
+                    #name(#(#argtypes::from_str(&argv.pop().unwrap()).unwrap(),)*);
+                }
+            },
+            _ => quote! { 
+                unsafe fn #handler(output: *mut libc::c_char, size: usize, args: Option<*mut *mut i8>, count: Option<usize>) {
+                    let argv: &[*mut libc::c_char; #argcount] = std::mem::transmute(args.unwrap());
+                    let mut argv: Vec<String> = argv.to_vec().into_iter().map(|s| std::ffi::CStr::from_ptr(s).to_str().unwrap().replace("\"", "")).collect();
+                    argv.reverse();
+                    let v = #name(#(#argtypes::from_str(&argv.pop().unwrap()).unwrap(),)*).to_string();
+                    libc::strncpy(output, std::ffi::CString::new(v).unwrap().into_raw(), size);
+                }
+            }
+        }
+    };
+
+    let expanded = quote! {
+        #[allow(non_upper_case_globals)]
+        static #info: FunctionInfo = FunctionInfo {
+            handler: #handler,
+            name: #sname,
+        };
+        #handlerfn
+        #ast
+    };
+
+    if args.is_empty() {
+        PROXIES.lock().unwrap().push(name.to_string());
+    } else {
+        PROXIES_ARG.lock().unwrap().push(name.to_string());
+    }
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn rv_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input!(item as ItemFn);
+
+    let proxies = (*PROXIES.lock().unwrap()).clone();
+    let info: Vec<syn::Ident> = proxies.iter().map(|s| {
+        syn::Ident::new(&format!("{}_info", s), proc_macro2::Span::call_site())
+    }).collect();
+    let proxies_arg = (*PROXIES_ARG.lock().unwrap()).clone();
+    let infoarg: Vec<syn::Ident> = proxies_arg.iter().map(|s| {
+        syn::Ident::new(&format!("{}_info", s), proc_macro2::Span::call_site())
+    }).collect();
+
+    let expanded = quote! {
+        use std::str::FromStr;
+
+        pub struct FunctionInfo {
+            name: &'static str,
+            handler: unsafe fn(*mut libc::c_char, usize, Option<*mut *mut i8>, Option<usize>) -> (),
+        }
+
+        static arma_proxies: &[&FunctionInfo] = &[#(&#info,)*];
+        static arma_proxies_arg: &[&FunctionInfo] = &[#(&#infoarg,)*];
+        static mut did_init: bool = false;
+
+        extern crate libc;
+        #[no_mangle]
+        pub unsafe extern "stdcall" fn RvExtensionVersion(output: *mut libc::c_char, output_size: usize) {
+            if !did_init {
+                init();
+                did_init = true;
+            }
+            let v = std::ffi::CString::new(env!("CARGO_PKG_VERSION")).unwrap().into_raw();
+            libc::strncpy(output, v, output_size);
+        }
+
+        #[no_mangle]
+        pub unsafe extern "stdcall" fn RVExtension(output: *mut libc::c_char, output_size: usize, function: *mut libc::c_char) {
+            let size = output_size - 1;
+            let r_function = std::ffi::CStr::from_ptr(function).to_str().unwrap();
+
+            for info in arma_proxies.iter() {
+                if info.name == r_function {
+                    (info.handler)(output, size, None, None);
+                    return;
+                }
+            }
+
+            libc::strncpy(output, std::ffi::CString::new("").unwrap().into_raw(), size);
+        }
+
+        #[no_mangle]
+        pub unsafe extern "stdcall" fn RVExtensionArgs(output: *mut libc::c_char, output_size: usize, function: *mut libc::c_char, args: *mut *mut libc::c_char, arg_count: usize) {
+            let size = output_size - 1;
+            let r_function = std::ffi::CStr::from_ptr(function).to_str().unwrap();
+            for info in arma_proxies_arg.iter() {
+                if info.name == r_function {
+                    (info.handler)(output, size, Some(args), Some(arg_count));
+                    return;
+                }
+            }
+        }
+
+        #ast
+    };
+
+    TokenStream::from(expanded)
+}
