1156ac79faeb6006129814e6a14eca9e1bfef2e0
Brett Mayson <brett@mayson.io>
2022-08-17 08:09:14 +0000

    test most of loadouts

---
+++ arma-rs/src/value/loadout/assigned.rs
@@ -0,0 +1,193 @@
+use crate::{FromArma, IntoArma, Value};
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+/// Assigned items in the loadout
+pub struct AssignedItems(String, String, String, String, String, String);
+impl AssignedItems {
+    /// The class name of the assigned map
+    #[must_use]
+    pub fn map(&self) -> &str {
+        &self.0
+    }
+
+    /// Set the class name of the assigned map
+    pub fn set_map(&mut self, map: String) {
+        self.0 = map;
+    }
+
+    /// The class name of the assigned terminal
+    #[must_use]
+    pub fn terminal(&self) -> &str {
+        &self.1
+    }
+
+    /// Set the class name of the assigned terminal
+    pub fn set_terminal(&mut self, terminal: String) {
+        self.1 = terminal;
+    }
+
+    /// The class name of the assigned radio
+    #[must_use]
+    pub fn radio(&self) -> &str {
+        &self.2
+    }
+
+    /// Set the class name of the assigned radio
+    pub fn set_radio(&mut self, radio: String) {
+        self.2 = radio;
+    }
+
+    /// The class name of the assigned compass
+    #[must_use]
+    pub fn compass(&self) -> &str {
+        &self.3
+    }
+
+    /// Set the class name of the assigned compass
+    pub fn set_compass(&mut self, compass: String) {
+        self.3 = compass;
+    }
+
+    /// The class name of the assigned watch
+    #[must_use]
+    pub fn watch(&self) -> &str {
+        &self.4
+    }
+
+    /// Set the class name of the assigned watch
+    pub fn set_watch(&mut self, watch: String) {
+        self.4 = watch;
+    }
+
+    /// The class name of the assigned NVG
+    #[must_use]
+    pub fn nvg(&self) -> &str {
+        &self.5
+    }
+
+    /// Set the class name of the assigned NVG
+    pub fn set_nvg(&mut self, nvg: String) {
+        self.5 = nvg;
+    }
+
+    /// Get all items
+    #[must_use]
+    pub fn items(&self) -> [&str; 6] {
+        [
+            self.map(),
+            self.terminal(),
+            self.radio(),
+            self.compass(),
+            self.watch(),
+            self.nvg(),
+        ]
+    }
+}
+impl FromArma for AssignedItems {
+    fn from_arma(s: String) -> Result<Self, String> {
+        <(String, String, String, String, String, String)>::from_arma(s).map(
+            |(map, gps, radio, compass, watch, nvg)| Self(map, gps, radio, compass, watch, nvg),
+        )
+    }
+}
+impl IntoArma for AssignedItems {
+    fn to_arma(&self) -> Value {
+        Value::Array(vec![
+            Value::String(self.map().to_owned()),
+            Value::String(self.terminal().to_owned()),
+            Value::String(self.radio().to_owned()),
+            Value::String(self.compass().to_owned()),
+            Value::String(self.watch().to_owned()),
+            Value::String(self.nvg().to_owned()),
+        ])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AssignedItems;
+    use crate::{FromArma, IntoArma, Value};
+
+    #[test]
+    fn map() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.map(), "");
+        assigned.set_map("map".to_owned());
+        assert_eq!(assigned.map(), "map");
+    }
+
+    #[test]
+    fn terminal() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.terminal(), "");
+        assigned.set_terminal("terminal".to_owned());
+        assert_eq!(assigned.terminal(), "terminal");
+    }
+
+    #[test]
+    fn radio() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.radio(), "");
+        assigned.set_radio("radio".to_owned());
+        assert_eq!(assigned.radio(), "radio");
+    }
+
+    #[test]
+    fn compass() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.compass(), "");
+        assigned.set_compass("compass".to_owned());
+        assert_eq!(assigned.compass(), "compass");
+    }
+
+    #[test]
+    fn watch() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.watch(), "");
+        assigned.set_watch("watch".to_owned());
+        assert_eq!(assigned.watch(), "watch");
+    }
+
+    #[test]
+    fn nvg() {
+        let mut assigned = AssignedItems::default();
+        assert_eq!(assigned.nvg(), "");
+        assigned.set_nvg("nvg".to_owned());
+        assert_eq!(assigned.nvg(), "nvg");
+    }
+
+    #[test]
+    fn from_arma() {
+        let s = "[\"map\",\"terminal\",\"radio\",\"compass\",\"watch\",\"nvg\"]";
+        let assigned = AssignedItems::from_arma(s.to_owned()).unwrap();
+        assert_eq!(assigned.map(), "map");
+        assert_eq!(assigned.terminal(), "terminal");
+        assert_eq!(assigned.radio(), "radio");
+        assert_eq!(assigned.compass(), "compass");
+        assert_eq!(assigned.watch(), "watch");
+        assert_eq!(assigned.nvg(), "nvg");
+    }
+
+    #[test]
+    fn to_arma() {
+        let mut assigned = AssignedItems::default();
+        assigned.set_map("map".to_owned());
+        assigned.set_terminal("terminal".to_owned());
+        assigned.set_radio("radio".to_owned());
+        assigned.set_compass("compass".to_owned());
+        assigned.set_watch("watch".to_owned());
+        assigned.set_nvg("nvg".to_owned());
+        let s = assigned.to_arma();
+        assert_eq!(
+            s,
+            Value::Array(vec![
+                Value::String("map".to_owned()),
+                Value::String("terminal".to_owned()),
+                Value::String("radio".to_owned()),
+                Value::String("compass".to_owned()),
+                Value::String("watch".to_owned()),
+                Value::String("nvg".to_owned()),
+            ])
+        );
+    }
+}
---
+++ arma-rs/src/value/loadout/inventory_item.rs
@@ -0,0 +1,185 @@
+use crate::{FromArma, IntoArma, Value};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+/// An item stored in a uniform, vest, or backpack
+pub enum InventoryItem {
+    /// An item that is not a magazine
+    Item(String, u32),
+    /// A magazine
+    Magazine(String, u32, u32),
+}
+impl InventoryItem {
+    /// Create a new item
+    #[must_use]
+    pub const fn new_item(class: String, count: u32) -> Self {
+        Self::Item(class, count)
+    }
+
+    /// Create a new magazine
+    #[must_use]
+    pub const fn new_magazine(class: String, count: u32, ammo: u32) -> Self {
+        Self::Magazine(class, count, ammo)
+    }
+
+    /// The item is a magazine
+    #[must_use]
+    pub const fn is_magazine(&self) -> bool {
+        matches!(self, Self::Magazine(_, _, _))
+    }
+
+    /// The class name of the item
+    #[must_use]
+    pub fn class(&self) -> &str {
+        match self {
+            Self::Item(c, _) | Self::Magazine(c, _, _) => c.as_str(),
+        }
+    }
+
+    /// Set the class name of the item
+    pub fn set_class(&mut self, class: String) {
+        match self {
+            Self::Item(c, _) | Self::Magazine(c, _, _) => *c = class,
+        }
+    }
+
+    /// The amount of the item
+    #[must_use]
+    pub fn count(&self) -> u32 {
+        match self {
+            Self::Item(_, c) | Self::Magazine(_, c, _) => c.to_owned(),
+        }
+    }
+
+    /// Set the amount of the item
+    pub fn set_count(&mut self, count: u32) {
+        match self {
+            Self::Item(_, c) | Self::Magazine(_, c, _) => *c = count,
+        }
+    }
+
+    /// The amount of ammo in the magazine
+    #[must_use]
+    pub fn ammo(&self) -> Option<u32> {
+        match self {
+            Self::Item(..) => None,
+            Self::Magazine(_, _, a) => Some(a.to_owned()),
+        }
+    }
+
+    /// Set the amount of ammo in the magazine
+    /// Returns true if the ammo was set, false if the item is not a magazine
+    pub fn set_ammo(&mut self, ammo: u32) -> bool {
+        match self {
+            Self::Magazine(_, _, a) => {
+                *a = ammo;
+                true
+            }
+            Self::Item(..) => false,
+        }
+    }
+}
+impl FromArma for InventoryItem {
+    fn from_arma(s: String) -> Result<Self, String> {
+        let commas = s.matches(',').count();
+        match commas {
+            1 => <(String, u32)>::from_arma(s).map(|(name, count)| Self::Item(name, count)),
+            2 => <(String, u32, u32)>::from_arma(s)
+                .map(|(name, count, ammo)| Self::Magazine(name, count, ammo)),
+            _ => Err(format!("Invalid inventory item: {}", s)),
+        }
+    }
+}
+impl IntoArma for InventoryItem {
+    fn to_arma(&self) -> Value {
+        match self {
+            Self::Item(name, count) => Value::Array(vec![
+                Value::String(name.clone()),
+                Value::Number(f64::from(*count)),
+            ]),
+            Self::Magazine(name, count, ammo) => Value::Array(vec![
+                Value::String(name.clone()),
+                Value::Number(f64::from(*count)),
+                Value::Number(f64::from(*ammo)),
+            ]),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{FromArma, IntoArma, Value};
+
+    use super::InventoryItem;
+
+    #[test]
+    fn is() {
+        let item = InventoryItem::new_item("test".to_owned(), 1);
+        assert_eq!(item.class(), "test");
+        assert_eq!(item.count(), 1);
+        assert_eq!(item.ammo(), None);
+        assert!(!item.is_magazine());
+        let item = InventoryItem::new_magazine("test".to_owned(), 1, 1);
+        assert_eq!(item.class(), "test");
+        assert_eq!(item.count(), 1);
+        assert_eq!(item.ammo(), Some(1));
+        assert!(item.is_magazine());
+    }
+
+    #[test]
+    fn class() {
+        let mut item = InventoryItem::new_item("test".to_owned(), 1);
+        assert_eq!(item.class(), "test");
+        item.set_class("test2".to_owned());
+        assert_eq!(item.class(), "test2");
+    }
+
+    #[test]
+    fn count() {
+        let mut item = InventoryItem::new_item("test".to_owned(), 1);
+        assert_eq!(item.count(), 1);
+        item.set_count(2);
+        assert_eq!(item.count(), 2);
+    }
+
+    #[test]
+    fn ammo() {
+        let item = InventoryItem::new_magazine("test".to_owned(), 1, 1);
+        assert_eq!(item.ammo(), Some(1));
+        assert!(item.is_magazine());
+        let item = InventoryItem::new_item("test".to_owned(), 1);
+        assert_eq!(item.ammo(), None);
+        assert!(!item.is_magazine());
+    }
+
+    #[test]
+    fn from_arma() {
+        let item = InventoryItem::from_arma("[test,1]".to_owned()).unwrap();
+        assert_eq!(item.class(), "test");
+        assert_eq!(item.count(), 1);
+        assert_eq!(item.ammo(), None);
+        assert!(!item.is_magazine());
+        let item = InventoryItem::from_arma("[test,1,1]".to_owned()).unwrap();
+        assert_eq!(item.class(), "test");
+        assert_eq!(item.count(), 1);
+        assert_eq!(item.ammo(), Some(1));
+        assert!(item.is_magazine());
+    }
+
+    #[test]
+    fn to_arma() {
+        let item = InventoryItem::new_item("test".to_owned(), 1);
+        assert_eq!(
+            item.to_arma(),
+            Value::Array(vec![Value::String("test".to_owned()), Value::Number(1.0),])
+        );
+        let item = InventoryItem::new_magazine("test".to_owned(), 1, 1);
+        assert_eq!(
+            item.to_arma(),
+            Value::Array(vec![
+                Value::String("test".to_owned()),
+                Value::Number(1.0),
+                Value::Number(1.0),
+            ])
+        );
+    }
+}
