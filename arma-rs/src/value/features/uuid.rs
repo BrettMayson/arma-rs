b6a02870d0eb6494f25c8062002ba30d65a8e0b3
BrettMayson <brett@mayson.io>
2023-02-04 04:18:38 -0600

    uuid from arma

--- arma-rs/src/value/features/uuid.rs
+++ arma-rs/src/value/features/uuid.rs
@@ -5,3 +5,9 @@ impl IntoArma for uuid::Uuid {
         self.to_string().to_arma()
     }
 }
+
+impl FromArma for uuid::Uuid {
+    fn from_arma(s: String) -> Result<Self, String> {
+        uuid::Uuid::parse_str(&s).map_err(|e| e.to_string())
+    }
+}
