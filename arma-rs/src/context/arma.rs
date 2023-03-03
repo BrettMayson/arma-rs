6b6f22b973ea01a18651dc678fcb45b4cca51c1c
Eathox <pgaholster@gmail.com>
2023-03-03 01:52:13 +0100

    Add simple unit tests

--- arma-rs/src/context/arma.rs
+++ arma-rs/src/context/arma.rs
@@ -9,23 +9,19 @@ pub struct ArmaContext {
 }
 
 impl ArmaContext {
-    #[must_use]
-    pub fn steam_id(&self) -> Option<&str> {
+    pub(crate) fn steam_id(&self) -> Option<&str> {
         self.steam_id.as_deref()
     }
 
-    #[must_use]
-    pub fn file_source(&self) -> Option<&Path> {
+    pub(crate) fn file_source(&self) -> Option<&Path> {
         self.file_source.as_deref()
     }
 
-    #[must_use]
-    pub fn mission_name(&self) -> Option<&str> {
+    pub(crate) fn mission_name(&self) -> Option<&str> {
         self.mission_name.as_deref()
     }
 
-    #[must_use]
-    pub fn server_name(&self) -> Option<&str> {
+    pub(crate) fn server_name(&self) -> Option<&str> {
         self.server_name.as_deref()
     }
 
@@ -67,5 +63,40 @@ impl ArmaContext {
             None
         };
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn steam_id_empty() {
+        let ctx = ArmaContext::default().with_steam_id("");
+        assert_eq!(ctx.steam_id(), None);
+    }
+
+    #[test]
+    fn steam_id_zero() {
+        let ctx = ArmaContext::default().with_steam_id("0");
+        assert_eq!(ctx.steam_id(), None);
+    }
+
+    #[test]
+    fn file_source_empty() {
+        let ctx = ArmaContext::default().with_file_source("");
+        assert_eq!(ctx.file_source(), None);
+    }
+
+    #[test]
+    fn mission_name_empty() {
+        let ctx = ArmaContext::default().with_mission_name("");
+        assert_eq!(ctx.mission_name(), None);
+    }
+
+    #[test]
+    fn server_name_empty() {
+        let ctx = ArmaContext::default().with_server_name("");
+        assert_eq!(ctx.server_name(), None);
     }
 }
