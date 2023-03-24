4829da8ff12a5c2a3edf4b664f35a4b49547ae48
Eathox <pgaholster@gmail.com>
2023-03-24 22:02:15 +0100

    Update example

--- arma-rs-example/src/counter.rs
+++ arma-rs-example/src/counter.rs
@@ -1,40 +1,45 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicU32, Ordering};
 
 use arma_rs::{Context, Group};
 
-pub struct Counter(pub AtomicUsize);
+pub struct Counter(pub AtomicU32);
 
 pub fn increment(ctx: Context) {
-    let counter = ctx.global().state().get::<Counter>();
+    let counter = ctx.state().get::<Counter>();
     counter.0.fetch_add(1, Ordering::SeqCst);
 }
 
+pub fn current(ctx: Context) -> u32 {
+    let counter = ctx.state().get::<Counter>();
+    counter.0.load(Ordering::SeqCst)
+}
+
 pub fn group() -> Group {
-    Group::new().command("increment", increment)
+    Group::new()
+        .command("increment", increment)
+        .command("current", current)
+        .state(Counter(0.into()))
 }
 
 #[cfg(test)]
 mod tests {
-    use std::sync::atomic::Ordering;
-
     use arma_rs::Extension;
 
     #[test]
     fn test_counter() {
         let extension = Extension::build()
-            .state(super::Counter(0.into()))
             .group("counter", super::group())
             .finish()
             .testing();
 
-        let (_, code) = unsafe { extension.call("counter:increment", None) };
-        let counter = extension.state().get::<super::Counter>();
+        unsafe { extension.call("counter:increment", None) };
+        let (result, code) = unsafe { extension.call("counter:current", None) };
         assert_eq!(code, 0);
-        assert_eq!(counter.0.load(Ordering::SeqCst), 1);
+        assert_eq!(result, "1");
 
-        let (_, code) = unsafe { extension.call("counter:increment", None) };
-        let counter = extension.state().get::<super::Counter>();
+        unsafe { extension.call("counter:increment", None) };
+        let (result, code) = unsafe { extension.call("counter:current", None) };
         assert_eq!(code, 0);
-        assert_eq!(counter.0.load(Ordering::SeqCst), 2);
+        assert_eq!(result, "2");
     }
 }
