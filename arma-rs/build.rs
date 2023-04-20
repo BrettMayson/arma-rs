60f321ad485481acc78718da9ea13cc558d70e55
Brett Mayson <brett@joinoffstreet.com>
2023-04-19 19:09:27 -0600

    skip skeptic

--- arma-rs/build.rs
+++ arma-rs/build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    if std::env::var("SKEPTIC_SKIP").unwrap_or("0".to_string()) == "1" {
+        return;
+    }
     let path = std::path::PathBuf::from("../README.md");
     if path.exists() {
         skeptic::generate_doc_tests(&[path]);
