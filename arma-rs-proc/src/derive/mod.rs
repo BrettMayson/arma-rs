990fc6a8d84085d56df2fdb5808b1b3890a6b3d9
Eathox <pgaholster@gmail.com>
2023-06-24 03:07:59 +0200

    Clean up generate functions

--- arma-rs-proc/src/derive/mod.rs
+++ arma-rs-proc/src/derive/mod.rs
@@ -9,49 +9,43 @@ use syn::{DeriveInput, Error, Result};
 
 use attr::*;
 use data::*;
-use struct_from::from_impl_body;
-use struct_into::into_impl_body;
 
 pub fn generate_into_arma(input: DeriveInput) -> Result<TokenStream> {
     let container = ContainerData::from_input(&input)?;
-    match container.data {
+    let body = match container.data {
         Data::Enum => Err(Error::new(Span::call_site(), "Enums aren't supported")),
-        Data::Struct(data) => {
-            let ident = container.ident;
-            let body = into_impl_body(&data, &container.attributes)?;
-            let (impl_generics, ty_generics, where_clause) = container.generics.split_for_impl();
-
-            Ok(quote! {
-                #[automatically_derived]
-                impl #impl_generics arma_rs::IntoArma for #ident #ty_generics #where_clause {
-                    fn to_arma(&self) -> arma_rs::Value {
-                        #body
-                    }
-                }
-            })
-        }
+        Data::Struct(data) => struct_into::into_impl_body(&data, &container.attributes),
         Data::Union => Err(Error::new(Span::call_site(), "Unions aren't supported")),
-    }
+    }?;
+
+    let ident = container.ident;
+    let (impl_generics, ty_generics, where_clause) = container.generics.split_for_impl();
+    Ok(quote! {
+        #[automatically_derived]
+        impl #impl_generics arma_rs::IntoArma for #ident #ty_generics #where_clause {
+            fn to_arma(&self) -> arma_rs::Value {
+                #body
+            }
+        }
+    })
 }
 
 pub fn generate_from_arma(input: DeriveInput) -> Result<TokenStream> {
     let container = ContainerData::from_input(&input)?;
-    match container.data {
+    let body = match container.data {
         Data::Enum => Err(Error::new(Span::call_site(), "Enums aren't supported")),
-        Data::Struct(data) => {
-            let ident = container.ident;
-            let body = from_impl_body(&data, &container.attributes)?;
-            let (impl_generics, ty_generics, where_clause) = container.generics.split_for_impl();
-
-            Ok(quote! {
-                #[automatically_derived]
-                impl #impl_generics arma_rs::FromArma for #ident #ty_generics #where_clause {
-                    fn from_arma(source: String) -> Result<Self, arma_rs::FromArmaError> {
-                        #body
-                    }
-                }
-            })
-        }
+        Data::Struct(data) => struct_from::from_impl_body(&data, &container.attributes),
         Data::Union => Err(Error::new(Span::call_site(), "Unions aren't supported")),
-    }
+    }?;
+
+    let ident = container.ident;
+    let (impl_generics, ty_generics, where_clause) = container.generics.split_for_impl();
+    Ok(quote! {
+        #[automatically_derived]
+        impl #impl_generics arma_rs::FromArma for #ident #ty_generics #where_clause {
+            fn from_arma(source: String) -> Result<Self, arma_rs::FromArmaError> {
+                #body
+            }
+        }
+    })
 }
